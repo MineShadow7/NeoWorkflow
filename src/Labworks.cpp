d990385b322b0253a4602ba2781925d96420837d
MineShadow <shad-karagodin@ya.ru>
2024-02-20 18:20:24 +0300

    Initial Source Files Commit

---
+++ src/Labworks.cpp
@@ -0,0 +1,23 @@
+﻿// Labworks.cpp: определяет точку входа для приложения.
+//
+
+#include <wx/wx.h>
+#include <wx/image.h>
+#include "cMain.h"
+
+
+class cMain: public wxApp {
+public:
+    bool OnInit();
+};
+
+IMPLEMENT_APP(cMain)
+
+bool cMain::OnInit()
+{
+    wxInitAllImageHandlers();
+    MyFrame* frame = new MyFrame(NULL, wxID_ANY, wxEmptyString);
+    SetTopWindow(frame);
+    frame->Show();
+    return true;
+}
